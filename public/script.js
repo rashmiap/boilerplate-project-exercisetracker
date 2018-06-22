b38f87a575958321fe22ac3000487c1f8a80c0d2
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-06-22 12:58:18 +0800

    Add iFrame reload function

--- public/script.js
+++ public/script.js
@@ -3,6 +3,7 @@ const openSlideButton = document.getElementById('menu-small')
 const closeSlideButton = document.getElementById('close-slide')
 const sideNavLink = document.getElementsByClassName('side-nav-link')
 const iframe = document.getElementById('formsFrame')
+const resetButton = document.getElementsByClassName('reset-btn')
 const homeButton = document.getElementsByClassName('home-btn')
 const addUserButton = document.getElementsByClassName('add-user-btn')
 const logExerciseButton = document.getElementsByClassName('exercise-btn')
@@ -73,7 +74,6 @@ const hideElement = () => {
 }
 
 const showElement= (item) => {
-  
   return () => {
     let element = iframe.contentWindow.document.querySelector(`${item}`)
     if (iframe.contentWindow.document.querySelector(`.visible`)) {
@@ -88,6 +88,14 @@ for (let i = 0; i < document.getElementsByClassName('nav-link').length; i++) {
   let element = document.getElementsByClassName('nav-link')[i]
   let referenceElem = document.getElementsByClassName('nav-link')[i].getAttribute('href')
   element.addEventListener("click", showElement(referenceElem))
+}
+
+const reloadIFrame = () => {
+  iframe.contentWindow.location.reload();
+}
+
+for (let i = 0; i < resetButton.length; i++) {
+  resetButton[i].addEventListener("click", reloadIFrame)
 }
 
 
